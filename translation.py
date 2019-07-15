266621668129e6a9dce7333ba16c6813b6a9a590
Yuya Kobayashi <ykobayashi@fun.bio.keio.ac.jp>
2019-07-15 16:11:21 +0900

    translation.py: google api を使用

--- translation.py
+++ translation.py
@@ -1,12 +1,16 @@
 #! /Users/ykobayashi/.pyenv/shims/python
-import sys
 import argparse
-import requests
 import re
+import sys
 import unicodedata
+from typing import Optional
+
+import pyperclip
+import requests
+from googletrans import Translator
 
 
-def translate_english(string: str):
+def _translate_english(string: str):
     url = 'https://translate.google.com/?hl=ja#en/ja/'
     headers = {"User-Agent": "Chrome/58.0.3029.110"}
     params = {'q': string}
@@ -18,7 +22,7 @@ def translate_english(string: str):
     return result
 
 
-def translate_japanese(string: str):
+def _translate_japanese(string: str):
     url = 'https://translate.google.co.jp/?hl=en#ja/en/'
     headers = {"User-Agent": "Chrome/58.0.3029.110"}
     params = {'q': string}
@@ -45,10 +49,11 @@ def main():
     parser = argparse.ArgumentParser(description='標準入力をGoogle翻訳で和訳')
     parser.add_argument('--input_file', '-i', nargs='?', default=None, help='入力ファイル')
     parser.add_argument('--output_file', '-o', nargs='?', default=None, help='出力ファイル')
-    parser.add_argument('--j2e', '-j', action='store_true', help='英訳フラグ')
+    parser.add_argument('--j2e', '-j', action='store_true', help='和文英訳フラグ')
 
     args = parser.parse_args()
 
+    input_text: Optional[str] = None
     if args.input_file is None:
         print('input text here.')
         input_text = sys.stdin.read()
@@ -63,18 +68,22 @@ def main():
 
     input_text = input_text.replace('\n', ' ')
 
+    if not input_text:
+        exit(0)
+
     if args.j2e:
-        result_text = translate_japanese(input_text)
+        result_text = Translator().translate(input_text, src="ja", dest="en").text
     elif is_japanese(input_text):
-        result_text = translate_japanese(input_text)
+        result_text = Translator().translate(input_text, src="ja", dest="en").text
     else:
-        result_text = translate_english(input_text)
+        result_text = Translator().translate(input_text, src="en", dest="ja").text
 
     if args.output_file:
         with open(args.output_file, 'w') as f:
             f.write(result_text)
     else:
         print(result_text + '\n')
+        pyperclip.copy(result_text)
 
 
 if __name__ == '__main__':
